9ae113b92d66ee78e315eba2f68e2465f283b8aa
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-12-05 14:02:39 +1300

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
 const data = [
   { id: "OLL21", alg: ["(R U2 R') (U' R U R') (U' R U' R')", "y (R U R' U) (R U' R' U) (R U2' R')"] },
-  { id: "OLL22", alg: ["r,r,r"] },
-  { id: "OLL23", alg: ["r,r,r"] },
-  { id: "OLL24", alg: ["r,r,r"] },
-  { id: "OLL25", alg: ["r,r,r"] },
+  { id: "OLL22", alg: ["R U2' R2' U' R2 U' R2' U2' R "] },
+  { id: "OLL23", alg: ["R2 D (R' U2 R) D' (R' U2 R')", "y2 R2' D' (R U2 R') D (R U2 R)"] },
+  { id: "OLL24", alg: ["(r U R' U') (r' F R F')", "y (R U R D) (R' U' R D') R2'  "] },
+  { id: "OLL25", alg: ["y F' (r U R' U') r' F R", "x (R' U R) D' (R' U' R) D x'"] },
   { id: "OLL26", alg: ["R U R' U R U2' R'", "y' R' U2' R U R' U R"] },
   { id: "OLL27", alg: ["R U2 R' U' R U' R'", "y' R' U' R U' R' U2 R"] }
 ];
