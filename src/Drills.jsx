f40e2f1d7bd3a0e124622783e5e5d61edea843bb
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-08 21:21:06 +1300

    need to add timer now

--- src/Drills.jsx
+++ src/Drills.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
 
 class Drills extends Component {
+  state = {
+    quantity: 0,
+    begin: false
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      begin: true
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      quantity: e.target.value
+    });
+  };
+
   render() {
     return (
       <div>
         <p>Welcome to Drills</p>
+        <form>
+          <input onChange={this.handleChange} type="number" />
+          <input
+            onClick={this.handleSubmit}
+            type="Submit"
+            value="Start Drills"
+          />
+        </form>
+        {this.state.begin && (
+          <div>
+            <h3>Rounds remaining {this.state.quantity}</h3>
+          </div>
+        )}
       </div>
     );
   }
