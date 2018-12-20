8c23428128bf80b7409388133aababfc6af8bd19
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-20 21:25:37 +1300

    data for knight shapes

---
+++ src/Data/KnightShapesData.js
@@ -0,0 +1,8 @@
+const data = [
+  { id: "OLL13", alg: ["r U' r' U' r U r' y' [R' U R]"] },
+  { id: "OLL14", alg: ["R' F R U R' F' R y' [R U' R']"] },
+  { id: "OLL15", alg: ["[l' U' l] [L' U' L U] [l' U l]"] },
+  { id: "OLL16", alg: ["[r U r'] [R U R' U'] [r U' r']"] }
+];
+
+export default data;
