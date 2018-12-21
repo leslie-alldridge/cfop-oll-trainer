ca462e020a9d35c732f278ad9cc66cd87a6bbc59
leslie-alldridge <leslie.alldridge@gmail.com>
2018-12-21 14:03:53 +1300

    data small bolts added

---
+++ src/Data/SmallBoltsData.js
@@ -0,0 +1,9 @@
+const data = [
+    { id: "OLL08", alg: ["R U2' R' U2 R' F R F'", "(y2) r' U' R U' R' U2 r"] },
+    { id: "OLL07", alg: ["[r U R' U] [R U2' r']"] },
+    { id: "OLL12", alg: ["[F R U R' U' F'] U [F R U R' U' F']", "(y) M U2 [R' U' R U' R' U2 R] U M'"] },
+    { id: "OLL11", alg: ["[F' L' U' L U F] y [F R U R' U' F']", "y [r U R' U] [R' F R F'] [R U2 r']", "(y') r' R2 U R' U [R U2 R' U] M'"] }
+  ];
+  
+export default data;
+  
